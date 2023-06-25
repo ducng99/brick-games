0cc7dbab189275ea371ac43abaa6f3f233cd2bee
ducng99 <tom@ducng.dev>
2023-06-25 13:22:35 +1200

    Added block style settings

--- src/components/Block.svelte
+++ src/components/Block.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { blocksBackground, blocksTransition } from '../stores/SettingsStore';
     import type CBlock from './CBlock';
 
     export let blockInfo: CBlock;
@@ -6,20 +7,35 @@
     const isOn = blockInfo.on;
 </script>
 
-<div class="brick" class:on={$isOn} />
+<div class="brick" class:on={$isOn} class:no-bg={!$blocksBackground} class:no-transition={!$blocksTransition} />
 
 <style lang="scss">
     .brick {
-        width: 3em;
-        height: 3em;
+        --game-block-min-opacity: 0.05;
+        --game-block-on-transition-time: 66ms;
+        --game-block-off-transition-time: 99ms;
+
+        width: var(--game-block-size);
+        height: var(--game-block-size);
         background: black;
-        opacity: 0.05;
-        border-radius: 0.2em;
-        box-shadow: inset 0px 0px 0.5px 2px black,
-            inset 0px 0px 0px 0.32em var(--game-bg);
+        opacity: var(--game-block-min-opacity);
+        border: 0.1em solid var(--game-bg);
+        box-shadow: inset 0 0 0.5px 0.1em black,
+            inset 0 0 0 0.5em var(--game-bg);
+        transition: opacity var(--game-block-off-transition-time) ease-out;
 
         &.on {
             opacity: 1;
+            transition: opacity var(--game-block-on-transition-time) ease-out;
+        }
+
+        &.no-bg {
+            --game-block-min-opacity: 0;
+        }
+
+        &.no-transition {
+            --game-block-on-transition-time: 0ms;
+            --game-block-off-transition-time: 0ms;
         }
     }
 </style>
---
+++ src/stores/SettingsStore.ts
@@ -0,0 +1,4 @@
+import { writable } from 'svelte/store';
+
+export const blocksBackground = writable(true);
+export const blocksTransition = writable(true);
