e1cdf903695a5da3ba649708e6160ee9a2424970
ducng99 <tom@ducng.dev>
2023-06-27 00:26:09 +1200

    Added score text

--- src/libs/Sidebar.svelte
+++ src/libs/Sidebar.svelte
@@ -6,6 +6,14 @@
 
 <div id="sidebar">
     <div id="score">{$score ?? 0}</div>
+    <div class="text">SCORE
+        <span id="soundIcon">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-music-note-beamed" viewBox="0 0 16 16">
+                <path d="M6 13c0 1.105-1.12 2-2.5 2S1 14.105 1 13c0-1.104 1.12-2 2.5-2s2.5.896 2.5 2zm9-2c0 1.105-1.12 2-2.5 2s-2.5-.895-2.5-2 1.12-2 2.5-2 2.5.895 2.5 2z"/>
+                <path fill-rule="evenodd" d="M14 11V2h1v9h-1zM6 3v10H5V3h1z"/>
+                <path d="M5 2.905a1 1 0 0 1 .9-.995l8-.8a1 1 0 0 1 1.1.995V3L5 4V2.905z"/>
+            </svg>
+        </span></div>
 </div>
 
 <style lang="scss">
@@ -19,5 +27,21 @@
         font-size: calc(var(--game-block-size));
         text-align: right;
         width: 100%;
+    }
+
+    .text {
+        font-family: 'JetbrainsMono';
+        font-size: 2em;
+        font-weight: bold;
+        margin: 1em 0;
+
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+    }
+
+    #soundIcon {
+        margin: 0 0.5em;
+        display: flex;
     }
 </style>
